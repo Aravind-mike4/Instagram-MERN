089d459775c50327d2eeb59f307a85bc175a8f03
Aravind-mike4 <pallepoguaravind3200@gmail.com>
2025-11-06 09:51:20 +0530

    Fix formatting in keys.js

--- backend/keys.js
+++ backend/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoUrl: "mongodb+srv://pallepoguaravind3200_db_user:v2WBmd3NKm7uVpAc@cluster0.6c6irz2.mongodb.net/?appName=Cluster0",
-   
+    mongoUrl: "mongodb+srv://pallepoguaravind3200_db_user:v2WBmd3NKm7uVpAc@cluster0.6c6irz2.mongodb.net/?appName=Cluster0"
+
 
 };
